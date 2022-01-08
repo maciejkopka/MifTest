64943f7798e4e7e9a7c538769efabec349a9fba4
Filip Czaplejewski <czafilpl@icloud.com>
2022-01-08 19:17:05 +0100

    commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>MIFBanking - panel konta</title>
+
+  </head>
+
+  <body>
+      <section style="padding-top: 100px">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2 text-center">
+                <h1 style="font-size: 160px">404</h1>
+                <h2>Strona nie istnieje</h2>
+                <p>Błędna nazwa strony</p>
+                <a href="/" class="btn btn-primary">Strona główna</a>
+            </div>
+        </div>
+    </div>
+</section>
+  </body>
